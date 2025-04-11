25c9fcf706d8b3daff0609ceb1630a1c45b3f65f
Cedric Girard <x-dark@valinor.fr>
2025-04-11 16:34:16 +0200

    Neovim -> lspconfig: add dockerls

--- nvim/dot-config/nvim/lua/plugins/nvim-lsp.lua
+++ nvim/dot-config/nvim/lua/plugins/nvim-lsp.lua
@@ -100,6 +100,7 @@ local M = {
           },
         },
         bashls = {},
+        dockerls = {},
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
