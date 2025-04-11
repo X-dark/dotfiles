b4112d59537ec5f528f119cf88b678fd58bc7b41
Cedric Girard <x-dark@valinor.fr>
2025-04-11 12:22:39 +0200

    Neovim: add telescope

---
+++ nvim/dot-config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,41 @@
+local M = {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require("telescope.builtin").find_files({
+          follow = true,
+        })
+      end
+    },
+    {
+      "<leader>fg",
+      function()
+        require("telescope.builtin").live_grep({
+          additional_args = {
+            "--follow",
+          }
+        })
+      end
+    },
+    {
+      "<leader>fb",
+      function()
+        require("telescope.builtin").buffers({
+          ignore_current_buffer = true,
+        })
+      end
+    },
+    {
+      "<leader>fh",
+      function()
+        require("telescope.builtin").help_tags()
+      end
+    },
+  },
+}
+
+return { M }
