6a9829016a002ec628a3d3041a89448e5a4b9372
Cedric Girard <x-dark@valinor.fr>
2025-04-11 14:39:26 +0200

    Neovim -> nvim-lsp: add bashls

--- nvim/dot-config/nvim/lua/plugins/nvim-lsp.lua
+++ nvim/dot-config/nvim/lua/plugins/nvim-lsp.lua
@@ -99,6 +99,7 @@ local M = {
             },
           },
         },
+        bashls = {},
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
