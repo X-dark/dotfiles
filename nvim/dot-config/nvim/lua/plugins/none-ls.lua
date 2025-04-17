213612ff5a345d1f73151fe1f0d864a50eb257cd
Cedric Girard <x-dark@valinor.fr>
2025-04-17 14:50:52 +0200

    Nvim -> none-ls: add printenv

--- nvim/dot-config/nvim/lua/plugins/none-ls.lua
+++ nvim/dot-config/nvim/lua/plugins/none-ls.lua
@@ -1,10 +1,11 @@
 local M = {
-  'nvimtools/none-ls.nvim',
+  "nvimtools/none-ls.nvim",
   opts = function()
     local null_ls = require("null-ls")
     local ret = {
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.hover.printenv,
       },
     }
     return ret
