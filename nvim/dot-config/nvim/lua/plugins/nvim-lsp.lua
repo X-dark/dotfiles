34f1f0ed70d2d33616ced71f2d8428ae1603cc47
Cedric Girard <x-dark@valinor.fr>
2025-04-17 15:26:41 +0200

    Nvim -> nvim-lspconfig: use virtual lines

--- nvim/dot-config/nvim/lua/plugins/nvim-lsp.lua
+++ nvim/dot-config/nvim/lua/plugins/nvim-lsp.lua
@@ -9,14 +9,7 @@ local M = {
       diagnostics = {
         underline = true,
         update_in_insert = false,
-        virtual_text = {
-          spacing = 4,
-          source = "if_many",
-          prefix = "●",
-          -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-          -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-          -- prefix = "icons",
-        },
+        virtual_lines = true,
         severity_sort = true,
         signs = {
           text = {
