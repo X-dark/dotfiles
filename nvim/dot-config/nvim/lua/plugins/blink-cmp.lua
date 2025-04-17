a2899613d7f267c17109dfe2a0b0027870a85bfe
Cedric Girard <x-dark@valinor.fr>
2025-04-17 14:49:46 +0200

    Nvim -> blink-cmp: customize conf

--- nvim/dot-config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/dot-config/nvim/lua/plugins/blink-cmp.lua
@@ -1,10 +1,10 @@
 local M = {
-  'saghen/blink.cmp',
+  "saghen/blink.cmp",
   dependencies = {
-    'rafamadriz/friendly-snippets',
+    "rafamadriz/friendly-snippets",
   },
 
-  version = '1.*',
+  version = "1.*",
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -15,24 +15,38 @@ local M = {
     -- C-n/C-p or Up/Down: Select next/previous item
     -- C-e: Hide menu
     -- C-k: Toggle signature help (if signature.enabled = true)
-    keymap = { preset = 'default' },
+    keymap = { preset = "default" },
 
     appearance = {
-      nerd_font_variant = 'mono'
+      nerd_font_variant = "mono",
     },
 
-    completion = { documentation = { auto_show = false } },
+    completion = {
+      documentation = {
+        auto_show = true,
+      },
+      ghost_text = { enabled = true },
+      menu = {
+        draw = {
+          columns = {
+            { "kind_icon" },
+            { "label", "label_description", gap = 1 },
+            { "source_name", gap = 3 },
+          },
+          treesitter = { "lsp" },
+        },
+      },
+    },
 
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { "lsp", "path", "snippets", "buffer" },
     },
 
     fuzzy = { implementation = "prefer_rust_with_warning" },
 
     signature = { enabled = true },
-
   },
-  opts_extend = { "sources.default" }
+  opts_extend = { "sources.default" },
 }
 
 return { M }
