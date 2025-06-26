4fa965dac25b4642d06b878db6cfc7457dc4607c
Cedric Girard <x-dark@valinor.fr>
2025-06-26 16:30:41 +0200

    Nvim -> Lualine: show parent dir

--- nvim/dot-config/nvim/lua/plugins/lualine.lua
+++ nvim/dot-config/nvim/lua/plugins/lualine.lua
@@ -1,13 +1,39 @@
 local M = {
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       options = {
-        theme = 'catppuccin',
-      }
-    }
-  }
+        theme = "catppuccin",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {
+          {
+            "filename",
+            path = 4,
+          },
+        },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          {
+            "filename",
+            path = 4,
+          },
+        },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+    },
+  },
 }
 
 return { M }
