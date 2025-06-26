b89bd76d6ec2835df19c9484fb882af602d473af
Cedric Girard <x-dark@valinor.fr>
2025-06-26 16:32:14 +0200

    Nvim -> neotree: git commands

--- nvim/dot-config/nvim/lua/plugins/neo-tree.lua
+++ nvim/dot-config/nvim/lua/plugins/neo-tree.lua
@@ -11,6 +11,16 @@ local M = {
   ---@type neotree.Config?
   opts = {
     close_if_last_window = true,
+    window = {
+      mappings = {
+        ["gu"] = "git_unstage_file",
+        ["ga"] = "git_add_file",
+        ["gr"] = "git_revert_file",
+        ["gc"] = "git_commit",
+        ["gp"] = "git_push",
+        ["gg"] = "git_commit_and_push",
+      },
+    },
   },
   keys = {
     {
