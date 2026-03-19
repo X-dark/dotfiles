fe3676e02b2fbf2614a149df7e9375e34d24c83c
Cedric Girard <533161+X-dark@users.noreply.github.com>
2026-03-19 09:58:30 +0100

    nvim: update telescope

--- nvim/dot-config/nvim/lua/plugins/telescope.lua
+++ nvim/dot-config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,11 @@
 local M = {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+
   keys = {
     {
       "<leader>ff",
