c71e039f6d657076a47366040cff4a7b6ff2adbe
Cedric Girard <533161+X-dark@users.noreply.github.com>
2026-04-16 10:28:06 +0200

    nvim: update treesiter for using rewrite correctly

--- nvim/dot-config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/dot-config/nvim/lua/plugins/nvim-treesitter.lua
@@ -10,6 +10,45 @@ local M = {
       additional_vim_regex_highlighting = false,
     },
   },
+  config = function()
+    local ts = require("nvim-treesitter")
+
+    ts.setup({
+      -- optional: install directory, logging, etc.
+    })
+
+    ts.install({
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+  end,
 }
 
 return { M }
